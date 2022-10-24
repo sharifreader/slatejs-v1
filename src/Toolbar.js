6377d25b0bc9029d5a3eee858013b36239d23289
sharifreader <sharifreader@gmail.com>
2022-10-25 00:41:54 +0100

    toolbarButtonArray

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -1,36 +1,23 @@
-import { Editor, Text, Transforms } from "slate";
-import { useSlateStatic } from "slate-react";
 import { ToolbarButtons } from "./ToolbarButtons";
 
 // Setting Properties from the Toolbar Buttons to be Rendered in the App.js
 
 export const Toolbar = () => {
-  const editor = useSlateStatic();
+  const toolbarButtonArray = [
+    { textEffect: "bold", buttonLabel: "b" },
+    { textEffect: "italic", buttonLabel: "i" },
+    { textEffect: "underline", buttonLabel: "_" },
+    { textEffect: "code", buttonLabel: "<>" },
+  ];
 
   return (
     <>
-      <button
-        onClick={(e) => {
-          const [match] = Editor.nodes(editor, {
-            match: (n) => Text.isText(n) && n.bold,
-          });
-          Transforms.setNodes(
-            editor,
-            { bold: !match },
-            {
-              match: (n) => Text.isText(n),
-              split: true,
-            }
-          );
-        }}
-      >
-        B
-      </button>
-
-      <ToolbarButtons textEffect="bold" buttonLabel="b" />
-      <ToolbarButtons textEffect="italic" buttonLabel="i" />
-      <ToolbarButtons textEffect="code" buttonLabel="<>" />
-      <ToolbarButtons textEffect="underline" buttonLabel="_" />
+      {toolbarButtonArray.map((n) => (
+        <ToolbarButtons
+          textEffect={`${n.textEffect}`}
+          buttonLabel={`${n.buttonLabel}`}
+        />
+      ))}
     </>
   );
 };
